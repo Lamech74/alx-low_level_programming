d52e1f423b0ef5e0545bde9535e7e8a179b09c1a
Lamech74 <omwegalamech@gmail.com>
2022-10-18 10:20:02 -0700

    task 6 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,49 +8,12 @@
  * Description: A fuction that computes the absolute value of an integer.
  *
  * Return: int
-#include "main.h"
  */
 
-
-int _abs(int number)
-/**
-
-{
- * _abs - Code entry point.
-
-	int result;
- *
-
-
-	if (number < 0)
- * @number: number is a positive or negative integer
-
-	{
- *
-
-		result = number * -1;
- * Description: A fuction that computes the absolute value of an integer.
-
-		return (result);
- *
-
-	}
-
-	else
- * Return: int
-
-	{
- */
-
-		result = number;
 int _abs(int number)
 
-		return (result);
 {
 
-	}
-
-}
 	int result;
 
 
