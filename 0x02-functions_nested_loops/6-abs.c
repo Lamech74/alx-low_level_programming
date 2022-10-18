4156f19d888cf13d76dbd35183b668f91fa26848
Lamech74 <omwegalamech@gmail.com>
2022-10-18 10:14:02 -0700

    task 6 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,77 @@
+#include "main.h"
+
+/**
+ * _abs - Code entry point.
+ *
+ * @number: number is a positive or negative integer
+ *
+ * Description: A fuction that computes the absolute value of an integer.
+ *
+ * Return: int
+#include "main.h"
+ */
+
+
+int _abs(int number)
+/**
+
+{
+ * _abs - Code entry point.
+
+	int result;
+ *
+
+
+	if (number < 0)
+ * @number: number is a positive or negative integer
+
+	{
+ *
+
+		result = number * -1;
+ * Description: A fuction that computes the absolute value of an integer.
+
+		return (result);
+ *
+
+	}
+
+	else
+ * Return: int
+
+	{
+ */
+
+		result = number;
+int _abs(int number)
+
+		return (result);
+{
+
+	}
+
+}
+	int result;
+
+
+	if (number < 0)
+
+	{
+
+		result = number * -1;
+
+		return (result);
+
+	}
+
+	else
+
+	{
+
+		result = number;
+
+		return (result);
+
+	}
+
+}
